885cb32d794e909a058e9ca9c8a0374b6eea0178
KaterinaTzannes <katerina.tzannes1@marist.edu>
2017-01-30 14:13:18 -0500

    Update Lab_1.java

--- Lab_1.java
+++ Lab_1.java
@@ -2,6 +2,8 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Katerina Tzannes
+ * Lab_1
  */
 package lab_1;
 import java.util.Scanner;
